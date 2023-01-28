f5eefc80b8095d8b0d031d53380912f2e9462201
Glowit Labs <glowitsofware@gmail.com>
2023-01-28 18:35:24 +0530

    Sentry Integrated

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,18 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://0a6bba99cf194f7483f70e3c489ff8d1@o4504582273630208.ingest.sentry.io/4504582278086656",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
