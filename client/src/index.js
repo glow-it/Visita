2333b9cc944b1fa6185166e2d4fc1bd0c6447ee6
Glowit Labs <glowitsofware@gmail.com>
2023-01-29 09:13:07 +0530

    vercel web vitals integrated

--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,11 @@ import { BrowserRouter } from "react-router-dom";
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 import { inject } from '@vercel/analytics';
+import { sendToVercelAnalytics } from './vitals';
 
 Sentry.init({
   dsn: "https://0a6bba99cf194f7483f70e3c489ff8d1@o4504582273630208.ingest.sentry.io/4504582278086656",
   integrations: [new BrowserTracing()],
-
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
@@ -31,4 +31,4 @@ root.render(
   </ChakraProvider>
 );
 
-reportWebVitals();
+reportWebVitals(sendToVercelAnalytics);
